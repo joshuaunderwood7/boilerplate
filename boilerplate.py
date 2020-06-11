bdf1d0f0fc8a715049056c188979651d1e060f3e
Joshua Underwood <joshuaunderwood7@gmail.com>
2020-06-11 07:43:43 -0400

    upgrade to python 3 and fixed bugs.
    There were errors here, and also python 3 renamed the config parser class

--- boilerplate.py
+++ boilerplate.py
@@ -1,60 +1,76 @@
 import argparse
+import configparser
 import logging
 import os
 
-from ConfigParser import SafeConfigParser
 
 #datetime formats
-DTFMT = '%Y%m%d%H%M%S' # YYYYmmddHHMMSS datetime parse format
+DTFMT = '%Y-%m-%dT%H:%M:%S' # YYYY-mm-ddTHH:MM:SS datetime parse format
 
 description = """
-This is the default description.
+Execute PPM generation
+
+(Command line arguments override configuration file settings)
 """
 arg_parser = argparse.ArgumentParser( description=description, 
     formatter_class=argparse.ArgumentDefaultsHelpFormatter )
-
-arg_parser.add_argument('--log', action='store'     
-    , default='INFO' , type=str
-    , help="Log level"
-    , choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
-    )
-
-arg_parser.add_argument('--log_name', action='store'     
-    , default='LOG' , type=str
-    , help="Change log name"
-    )
 
 arg_parser.add_argument('--config_filename', action='store'     
     , default='config.ini' , type=str
     , help="Path of configuration file"
     )
 
-args   = None
-log    = None
-config = None
+arg_parser.add_argument('--logging.level', action='store'
+    , type=str
+    , help="Log level"
+    , choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+    )
+
+arg_parser.add_argument('--logging.name', action='store'     
+    , type=str
+    , help="Change log name"
+    )
+
+args          = None
+log           = None
+config        = None
+modified_time = 0
 
 def init():
-  global args
-  global log
-  global config
+    global args
+    global log
+    global config
+    global modified_time
 
-  args = arg_parser.parse_args()
+    args = arg_parser.parse_args()
 
-  logging.basicConfig( level   = getattr(logging, args.log, 'CRITICAL')
-                    , format  = '%(asctime)s %(levelname)-8s %(message)s'
-                    , datefmt = '%Y-%m-%d %H:%M:%S'
-                    )
+    config = configparser.ConfigParser()
+    config.read(args.config_filename)
+    modified_time = os.stat(args.config_filename).st_mtime
 
-  log    = logging.getLogger(name=args.name)
+    log_level = getattr(args, 'logging.level') or config.get('logging', 'level'   , fallback='CRITICAL')
+    log_name  = getattr(args, 'logging.name' ) or config.get('logging', 'log_name', fallback='LOG'     )
 
-  config = SafeConfigParser()
-  config.read(args.config_filename)
-  modified_time = os.stat(args.config_filename).st_mtime
+    logging.basicConfig( level   = log_level
+                       , format  = '%(asctime)s RSTORM-PPM:%(process)d %(levelname)-8s %(message)s'
+                       , datefmt = DTFMT
+                       )
+    log = logging.getLogger(name=log_name)
+
 
 def reload_config():
-  new_modified_time = os.stat(self.config_filename).st_mtime
-  if new_modified_time != modified_time:
-    log.info("Reloading configuration file {}".format(self.config_filename))
-    config.read(args.config_filename)
-    self.modified_time = new_modified_time
+    global modified_time
+    new_modified_time = os.stat(args.config_filename).st_mtime
+    if new_modified_time != modified_time:
+        log.info("Reloading configuration file {}".format(args.config_filename))
+        config.read(args.config_filename)
+        modified_time = new_modified_time
 
+
+if __name__=="__main__":
+    init()
+    reload_config()
+    log.info("PROGRAM START")
+    log.info("PROGRAM END")
+
+  
